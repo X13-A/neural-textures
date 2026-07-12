b3ace7a391f9603f9f22994cb004b001454bc6a3
Alex <foulonalex@yahoo.com>
2026-07-12 23:18:34 +0200

    Warp-aggregated float atomics (better)
    Still ~2x slower than integer path.

--- src/neural/nn_common.cuh
+++ src/neural/nn_common.cuh
@@ -1,6 +1,10 @@
 #pragma once
 #include <cuda_runtime.h>
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
 #include <cstdint>
+
+namespace cg = cooperative_groups;
 
 // Shared device code used by both the MLP and the hash grid
 
@@ -25,8 +29,19 @@ __device__ inline float rand_float(uint32_t& state)
 
 __device__ inline void accumulate_gradient(float4* gradient_buffer, uint32_t idx, float4 gradient)
 {
-    atomicAdd(&gradient_buffer[idx].x, gradient.x);
-    atomicAdd(&gradient_buffer[idx].y, gradient.y);
-    atomicAdd(&gradient_buffer[idx].z, gradient.z);
-    atomicAdd(&gradient_buffer[idx].w, gradient.w);
+    // Warp-aggregated float atomicAdd
+    auto group = cg::labeled_partition(cg::coalesced_threads(), idx);
+
+    float x = cg::reduce(group, gradient.x, cg::plus<float>());
+    float y = cg::reduce(group, gradient.y, cg::plus<float>());
+    float z = cg::reduce(group, gradient.z, cg::plus<float>());
+    float w = cg::reduce(group, gradient.w, cg::plus<float>());
+
+    if (group.thread_rank() == 0)
+    {
+        atomicAdd(&gradient_buffer[idx].x, x);
+        atomicAdd(&gradient_buffer[idx].y, y);
+        atomicAdd(&gradient_buffer[idx].z, z);
+        atomicAdd(&gradient_buffer[idx].w, w);
+    }
 }
